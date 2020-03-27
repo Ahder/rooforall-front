374d368182e738ebd9f36d63dd9c42dfe30724d6
Miguel Yann <tsambamiguelito@gmail.com>
2020-03-27 09:50:49 +0100

    WIP Sign up, REFACTO and test new library hooks for axios

--- src/App.js
+++ src/App.js
@@ -2,37 +2,55 @@ import React, {useState} from 'react';
 import './App.css';
 import SignUp from "./SignUp";
 import Button from "@material-ui/core/Button";
-import { usePostCallback } from "use-axios-react";
+import axios from 'axios';
 
 
 const title = 'Inscription sur RooforAll';
+const url = "http:localhost:8080/api/users/signUp";
 
-function userToRequest({ userName, password,   }) {
-    return {
-        url: "https://localhost:8080/api/users/signup",
-        data: { name, job }
-    };
-}
 function App () {
     const [username, setUserName] = useState('');
     const [mail, setMail] = useState('');
     const [password, setPassword] = useState('');
+    const data = {};
     
     
-    const signIp = (usernameInput, mailInput, password) => {
-        axios
+    const handlerUserName = (e) => {
+        setUserName(e.target.value);
+        console.log(e.tar.name);
     }
     
+    const handlerUsMail = (e) => {
+        setMail(e.target.value);
+        console.log(e.tar.name);
+        
+    }
+    
+    const handlerPwd = (e) => {
+        setPassword(e.target.value);
+        console.log(e.target.name);
+        
+    }
+    
+    
+    const signUp = (usernameInput, mailInput, password) => {
+        axios.post(url,)
+        
+    }
     
     
     return (
         <div>
             <h1 style={{textAlign: "center"}}>{title}</h1>
             <div className="App">
-                <SignUp defaultValue={"Enter username"}/>
-                <SignUp defaultValue={"Enter email"}/>
-                <SignUp defaultValue={"Enter password"}/>
-                <Button variant="contained" color="primary">Default</Button>
+                <SignUp defaultValue={"Enter username"}
+                        inputHandler={(e) => handlerUserName(e)}
+                />
+                <SignUp defaultValue={"Enter email"} inputHandler={(e) => handlerUsMail(e)}
+                />
+                <SignUp defaultValue={"Enter password"} inputHandler={(e) => handlerPwd(e)}
+                />
+                <Button onClick={signUp} variant="contained" color="primary">Default</Button>
             
             </div>
         
