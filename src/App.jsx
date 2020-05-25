56d845451e33decb57c7bda2eb8589e6ead745e8
Miguel Yann <tsambamiguelito@gmail.com>
2020-05-25 23:18:08 +0200

    (WIP) Add landing page for explain app

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import Register from './pages/register/Register';
 import Home from './pages/home/home.component';
 import './App.css';
 import Header from './components/header/header.component';
+import Landing from './pages/landing/landing.component';
 
 function App() {
   return (
     <div className="app-class">
       <Header />
       <Switch>
-        <Route exact path="/register" component={Register} />
+        <Route exact path="/" component={Landing} />
+        <Route path="/register" component={Register} />
         <Route path="/home" component={Home} />
       </Switch>
     </div>
---
+++ src/pages/landing/landing.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Landing() {
+  return (
+    <div>
+      <h2 style={{ color: 'red' }}>Toujours plus proche de vous</h2>
+    </div>
+  );
+}
+
+export default Landing;
