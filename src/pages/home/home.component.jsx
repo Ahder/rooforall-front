2ac3a2ff4ba5738ce137a95ce9475e5b34c6e85f
Miguel Yann <tsambamiguelito@gmail.com>
2020-06-02 23:18:04 +0200

    (UI) aAdd button for create record

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './home.styles.css';
-function Home({ location }) {
+
+function Home ({ location }) {
   console.log(location);
   return (
-    <div className="home">
-      <h1>
-        <span className="home-title">Hello</span> {location.state}
-      </h1>
-    </div>
+    <>
+      <div className="home">
+        <h1>
+          <span className="home-title">Hello</span> {location.state}
+        </h1>
+        <div className="home-choice">
+          <button className="btn-register-home" onClick={() =>{}}>
+            Creer un dossier
+          </button>
+        </div>
+      </div>
+    
+    </>
   );
 }
 
